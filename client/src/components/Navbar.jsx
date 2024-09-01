26e2423be02a5fc36c7f891526cfa08093c413e2
leejz <957675793@qq.com>
2024-09-01 15:26:54 +0800

    Navbar responsive design update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -28,11 +28,22 @@ const Navbar = () => {
                     ? <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer"
                                       onClick={() => setToggleMenu(false)}/>
                     : <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer"
-                                  onClick={() => setToggleMenu(true)}/>
-                }
+                                  onClick={() => setToggleMenu(true)}/>}
+                {toggleMenu && (
+                    <ul className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+                    flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in ">
+                        <li className="text-xl w-full my-2">
+                            <AiOutlineClose onClick={() => setToggleMenu(false)}/>
+                            {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+                                <NavbarItem key={item + index} title={item} classProps="my-2 text-lg"/>
+                            ))}
+                        </li>
+                    </ul>
+                )}
             </div>
         </nav>
     );
 }
 
 export default Navbar
+
