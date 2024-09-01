3d90cb0b70cd21c0ef75e3ba2d2fc7ee02b31c56
leejz <957675793@qq.com>
2024-09-01 14:48:30 +0800

    navbar change

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import {HiMenuAlt4} from 'react-icons/hi';
 import {AiOutlineClose} from 'react-icons/ai';
 import logo from '../../images/logo.png';
+import {useState} from "react";
 
 const NavbarItem = ({title, classProps}) => {
     return (
@@ -8,6 +9,7 @@ const NavbarItem = ({title, classProps}) => {
     )
 }
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
     return (
         <nav className="w-full flex md:justify-center justify-between items-center p-4">
             <div className="md:flex-[0.5] flex-initial justify-center items-center">
@@ -18,9 +20,17 @@ const Navbar = () => {
                     <NavbarItem key={item + index} title={item}/>
                 ))}
                 <li>
-                    <button className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">Login</button>
+                    <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">Login</li>
                 </li>
             </ul>
+            <div className="">
+                {toggleMenu
+                    ? <AiOutlineClose fontSize={40} className="text-white md:hidden cursor-pointer"
+                                      onClick={() => setToggleMenu(false)}/>
+                    : <HiMenuAlt4 fontSize={40} className="text-white md:hidden cursor-pointer"
+                                  onClick={() => setToggleMenu(true)}/>
+                }
+            </div>
         </nav>
     );
 }
